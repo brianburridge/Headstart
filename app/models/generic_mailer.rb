e6d9282b47379a83f7551524bd395b11cc08197e
Brian Burridge <bburridg@gmail.com>
2010-10-07 17:57:59 -0400

    Set content type to html to force html email

--- app/models/generic_mailer.rb
+++ app/models/generic_mailer.rb
@@ -1,5 +1,5 @@
 class GenericMailer < ActionMailer::Base
-
+  
   def change_password(user)
     from       Headstart.configuration.mailer_sender
     recipients user.email
@@ -9,6 +9,7 @@ class GenericMailer < ActionMailer::Base
     body       :url => edit_user_password_url(user,
                        :token  => user.password_reset_token,
                        :escape => false)
+    content_type "text/html"
   end
   
   def welcome(user)
@@ -18,14 +19,7 @@ class GenericMailer < ActionMailer::Base
                       :scope   => [:headstart, :models, :headstart_mailer],
                       :default => "Welcome")
     body      :user => user
+    content_type "text/html"
   end
 
-  def confirmation(user)
-    from       Headstart.configuration.mailer_sender
-    recipients user.email
-    subject    I18n.t(:confirmation,
-                      :scope   => [:headstart, :models, :headstart_mailer],
-                      :default => "Account confirmation")
-    body      :user => user
-  end
 end
